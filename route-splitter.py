988d30c057468f451c7d8acbfaeef910c9d9b7f1
bobbutnotbob <37612781+bobbutnotbob@users.noreply.github.com>
2019-05-08 10:31:09 +1200

    Added function to sort data
    Added a function to iterate through all the values and store the ones that contain "|"

--- route-splitter.py
+++ route-splitter.py
@@ -1,11 +1,20 @@
 # Program: Take data and refine it for easy entry into the database
 # Author: Bobbutnotbob (Emile)
-# Date: 03/05/2019
-# Version: 1.00
+# Date: 08/05/2019
+# Version: 1.10
 import pandas as pd
 import requests
 
 # Grab the data from our GitHub repository and store it locally so Panda can read it
 bus_stops_file = requests.get('https://raw.githubusercontent.com/b0bsky/Metro-Timetable-Application/map-database/Bus_Stops.csv', allow_redirects=True)
 open('bus_stops.csv', 'wb').write(bus_stops_file.content)
-data = pd.read_csv('bus_stops.csv', usecols=['Road', 'Routes'], dtype={"Road":str,"Routes":str})
+data = pd.read_csv('bus_stops.csv', dtype={"Routes":str}, index_col='Road')
+
+# Iterate through every row in the CSV and append each tuple with a "|" character into a list to make it easier to work with
+list_of_tuples_of_dictionaries = []
+for row in data.itertuples():
+    if "|" in row[4]:
+        list_of_tuples_of_dictionaries.append(row)
+
+print(list_of_tuples_of_dictionaries)
+
