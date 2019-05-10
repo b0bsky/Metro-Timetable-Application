746584f42af56fa8a6fd900679e75c9e07b91f3d
Reuben Maddock <34594488+b0bsky@users.noreply.github.com>
2019-05-10 12:16:14 +1200

    added outlines of search, query and results boxes

--- main_win.py
+++ main_win.py
@@ -9,12 +9,32 @@ from tkinter import Tk, Frame, Label
 root = Tk(className=" Metro Application")
 root.state("zoomed")
 
-# Metro title
-title_frame = Frame(height = "100", width = "1920", bg = "dark orchid")
-title_frame.grid(columnspan = 10)
+# -------- METRO TITLE --------- #
+# Metro title frame
+title_frame = Frame(root, height = "100", width = "1920", bg = "dark orchid")
+title_frame.grid(row = 0, columnspan = 8)
 
-title_text = Label(root, text = "RETRO TRANSPORT", fg = "maroon3", bg = "dark orchid", font = ("arial", 30))
-title_text.grid(row = 0, column = 3)
+# Metro title text
+title_text = Label(root, text = "RETRO TRANSPORT", fg = "maroon3", bg = "dark orchid", font = ("arial", 40))
+title_text.place(x = 500, y = 20)
+
+# ----- SEARCH BAR ------ #
+
+# Search bar frame
+search_bar_frame = Frame(root, height = "50", width = "499", borderwidth = 2, relief = "groove")
+search_bar_frame.grid(row = 1, column = 0, sticky = "nw")
+
+# ----- RESULTS BAR ----- #
+
+# Results bar frame
+results_bar_frame = Frame(root, width = "500", height = "300", borderwidth = 3, relief = "groove")
+results_bar_frame.grid(row = 2, column = 0, sticky = "nw")
+
+# ----- QUERY BAR ------- #
+
+# Query bar frame
+query_bar_frame = Frame(root, width = "500", height = "390", borderwidth = 3, relief = "groove")
+query_bar_frame.grid(row = 3, column = 0, sticky = "nw")
 
 # Main window loop
 root.mainloop()
