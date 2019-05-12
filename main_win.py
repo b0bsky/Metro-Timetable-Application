a56b6baa4aaf6273072551f379c2c0595cafc89a
Reuben Maddock <34594488+b0bsky@users.noreply.github.com>
2019-05-12 21:57:41 +1200

    improved layout, changed+added font/colours, began query section

--- main_win.py
+++ main_win.py
@@ -3,7 +3,7 @@
     Developer: Reuben Maddock
 '''
 
-from tkinter import Tk, Frame, Label
+from tkinter import Tk, Frame, Label, Entry, Listbox
 
 # Creating main window and setting window options
 root = Tk(className=" Metro Application")
@@ -15,8 +15,8 @@ title_frame = Frame(root, height = "100", width = "1920", bg = "dark orchid")
 title_frame.grid(row = 0, columnspan = 8)
 
 # Metro title text
-title_text = Label(root, text = "RETRO TRANSPORT", fg = "maroon3", bg = "dark orchid", font = ("arial", 40))
-title_text.place(x = 500, y = 20)
+title_text = Label(root, text = "RETRO TRANSPORT", fg = "maroon3", bg = "dark orchid", font = ("fixedsys", 40))
+title_text.grid(row = 0, column = 2)
 
 # ----- SEARCH BAR ------ #
 
@@ -24,17 +24,34 @@ title_text.place(x = 500, y = 20)
 search_bar_frame = Frame(root, height = "50", width = "499", borderwidth = 2, relief = "groove")
 search_bar_frame.grid(row = 1, column = 0, sticky = "nw")
 
+search_bar_entry = Entry(search_bar_frame)
+search_bar_entry.grid(row = 1, column = 0, sticky = "nw")
+
 # ----- RESULTS BAR ----- #
 
 # Results bar frame
-results_bar_frame = Frame(root, width = "500", height = "300", borderwidth = 3, relief = "groove")
+results_bar_frame = Frame(root, width = "500", height = "400", borderwidth = 3, relief = "groove")
 results_bar_frame.grid(row = 2, column = 0, sticky = "nw")
 
 # ----- QUERY BAR ------- #
 
 # Query bar frame
-query_bar_frame = Frame(root, width = "500", height = "390", borderwidth = 3, relief = "groove")
+query_bar_frame = Frame(root, width = "500", height = "500", bg = "dodgerblue3", borderwidth = 3, relief = "groove")
 query_bar_frame.grid(row = 3, column = 0, sticky = "nw")
+
+# Trip options label
+trip_options_label = Label(root, text = "TRIP OPTIONS:", fg = "black", bg = "dodgerblue3", font = ("verdana", 17))
+trip_options_label.grid(row = 3, column = 0, sticky = "nw", padx = 150, pady = 25)
+
+# From query
+from_label = Label(root, text = "FROM: ", fg = "black", bg = "dodgerblue3", font = ("verdana 12 bold"))
+from_label.place(x = 35, y = 630)
+
+from_query = Entry(root)
+from_query.place(x = 100, y = 630)
+
+from_listbox = Listbox(root)
+from_listbox.place(x = 100, y = 650)
 
 # Main window loop
 root.mainloop()
