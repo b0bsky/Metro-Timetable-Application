9c63dda1f957e15c577c09fe5123557192d93007
Reuben Maddock <34594488+b0bsky@users.noreply.github.com>
2019-05-17 23:32:02 +1200

    fixed major listbox issue

--- main_win.py
+++ main_win.py
@@ -43,6 +43,7 @@ class Query(Frame):
         Frame.__init__(self, master)
 
         self.query_widgets()
+        self.listbox_is_showing = False
 
     # Function that controls all query widgets
     def query_widgets(self):
@@ -62,31 +63,43 @@ class Query(Frame):
         # From query
         self.from_label = Label(root, text="FROM: ", fg="black", bg="dodgerblue3", font=("verdana 12 bold"))
         self.from_label.place(x=35, y=630)
-
         self.from_query = Entry(root, textvariable = self.users_search)
         self.from_query.place(x = 100, y = 632)
 
+        # Closing listboxes when focus is elsewhere
         self.from_query.bind("<FocusIn>", self.updateList)
 
     # Dynamically updates the query frames listboxes
     def updateList(self, *args):
 
+        # Gets users search(live)
         search_term = self.users_search.get()
 
+        # Puts listboxes on screen and makes them dissapear if users not using entry
         self.from_listbox = Listbox(root)
         self.from_listbox.place(x=100, y=652)
+        self.listbox_is_showing = True
         self.from_listbox.pi = self.from_listbox.place_info()
         self.from_listbox.config(highlightbackground="red")
+        self.from_listbox.bind("<FocusOut>", self.toggle_visibility)
 
         test_lbox = ['Adam', 'Lucy', 'Barry', 'Bob', 'James', 'Frank', 'Susan', 'Amanda', 'Christie']
 
+        # Dynamically prints out listbox items matching users search
         self.from_listbox.delete(0, END)
 
         for item in test_lbox:
             if search_term.lower() in item.lower():
                 self.from_listbox.insert(END, item)
-    def toggle(self):
-        print("clicked")
+
+    # Toggles listboxes visibility on screen
+    def toggle_visibility(self, event = None):
+        if self.listbox_is_showing:
+            self.from_listbox.place_forget()
+            self.listbox_is_showing = False
+        else:
+            self.from_listbox.place(x=100, y=652)
+            self.listbox_is_showing = True
 
 Query(master = root)
 
