eb36d9de0cea1be55f72a2d54284283665a4e147
Reuben Maddock <34594488+b0bsky@users.noreply.github.com>
2019-05-20 15:05:04 +1200

    improved searchbar/layout and fixed minor listbox error

--- main_win.py
+++ main_win.py
@@ -3,7 +3,7 @@
     Developer: Reuben Maddock
 '''
 
-from tkinter import Tk, Frame, Label, Entry, Listbox, StringVar, END
+from tkinter import Tk, Frame, Label, Entry, Listbox, StringVar, END, ttk
 
 # Creating main window and setting window options
 root = Tk(className=" Metro Application")
@@ -24,13 +24,16 @@ title_text.grid(row = 0, column = 2)
 search_bar_frame = Frame(root, height = "50", width = "499", borderwidth = 2, relief = "groove")
 search_bar_frame.grid(row = 1, column = 0, sticky = "nw")
 
-search_bar_entry = Entry(search_bar_frame, width = "82")
+search_bar_style = ttk.Style()
+search_bar_style.configure("TEntry", )
+
+search_bar_entry = Entry(search_bar_frame, width = "45", font = ("Helvetica 14"))
 search_bar_entry.grid(row = 1, column = 0, sticky = "w")
 
 # ----- RESULTS BAR ----- #
 
 # Results bar frame
-results_bar_frame = Frame(root, width = "500", height = "400", borderwidth = 3, relief = "groove")
+results_bar_frame = Frame(root, width = "500", height = "325", borderwidth = 3, relief = "groove")
 results_bar_frame.grid(row = 2, column = 0, sticky = "nw")
 
 # ----- QUERY BAR ------- #
@@ -62,11 +65,11 @@ class Query(Frame):
 
         # From query
         self.from_label = Label(root, text="FROM: ", fg="black", bg="dodgerblue3", font=("verdana 12 bold"))
-        self.from_label.place(x=35, y=630)
-        self.from_query = Entry(root, textvariable = self.users_search)
-        self.from_query.place(x = 100, y = 632)
+        self.from_label.place(x=35, y=555)
+        self.from_query = Entry(root, width = "35", textvariable = self.users_search)
+        self.from_query.place(x = 120, y = 557)
 
-        # Closing listboxes when focus is elsewhere
+        # Dynamically updates the from queries search
         self.from_query.bind("<FocusIn>", self.updateList)
 
     # Dynamically updates the query frames listboxes
@@ -75,30 +78,32 @@ class Query(Frame):
         # Gets users search(live)
         search_term = self.users_search.get()
 
-        # Puts listboxes on screen and makes them dissapear if users not using entry
-        self.from_listbox = Listbox(root)
-        self.from_listbox.place(x=100, y=652)
+        # Puts listboxes on screen and makes them disapear if users not using entry
+        self.from_listbox = Listbox(root, width = "35")
+        self.from_listbox.place(x=120, y=577)
         self.listbox_is_showing = True
         self.from_listbox.pi = self.from_listbox.place_info()
         self.from_listbox.config(highlightbackground="red")
+        self.from_query.bind("<FocusOut>", self.toggle_visibility)
         self.from_listbox.bind("<FocusOut>", self.toggle_visibility)
 
         test_lbox = ['Adam', 'Lucy', 'Barry', 'Bob', 'James', 'Frank', 'Susan', 'Amanda', 'Christie']
 
-        # Dynamically prints out listbox items matching users search
+        # Removes any past data left on from listbox
         self.from_listbox.delete(0, END)
 
+        # Dynamically prints out listbox items matching users search
         for item in test_lbox:
             if search_term.lower() in item.lower():
                 self.from_listbox.insert(END, item)
 
     # Toggles listboxes visibility on screen
-    def toggle_visibility(self, event = None):
-        if self.listbox_is_showing:
+    def toggle_visibility(self, event=None):
+        if self.listbox_is_showing and self.from_listbox is not self.from_listbox.focus_get():
             self.from_listbox.place_forget()
             self.listbox_is_showing = False
         else:
-            self.from_listbox.place(x=100, y=652)
+            self.from_listbox.place(x=120, y=577)
             self.listbox_is_showing = True
 
 Query(master = root)
