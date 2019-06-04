79f26956bbac5bf69a13bf8311b068c1bd3de5d4
Reuben Maddock <34594488+b0bsky@users.noreply.github.com>
2019-06-04 22:33:00 +1200

    frame testing

--- main_win.py
+++ main_win.py
@@ -4,24 +4,49 @@
 '''
 
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QCheckBox, QStyleFactory, QLabel
+from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QCheckBox, QStyleFactory, QLabel, QFrame, QHBoxLayout
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 
+
+# Main window class
 class Window(QMainWindow):
     def __init__(self):
         super(Window, self).__init__()
 
+        self.setup_main_window()
+        self.title_frame()
+
+        self.show()
+
+    # Setup main window options
+    def setup_main_window(self):
+
+        # Window settings
+        self.central_widget = QWidget()
+        self.setCentralWidget(self.central_widget)
         self.showMaximized()
         self.setWindowTitle("Test")
 
-    def titleFrame(self):
+    def title_frame(self):
+        # Box layout
+        hbox = QHBoxLayout(self.central_widget)
 
-        self.title_label = QLabel(self)
-        self.title_label.setText("RETRO TRANSPORT")
-        self.title_label.setAlignment()
+        # Title frame
+        self.title_frame = QFrame()
+        self.title_frame.setFrameShape(QFrame.StyledPanel)
+        self.title_frame.setLineWidth(0.6)
 
-app = QApplication(sys.argv)
-app.setStyle(QStyleFactory.create('Cleanlooks'))
-GUI = Window()
-sys.exit(app.exec_())
+        hbox.addWidget(self.title_frame)
+
+        self.setLayout(hbox)
+
+def main():
+    # Initiating program
+    app = QApplication(sys.argv)
+    app.setStyle(QStyleFactory.create('Cleanlooks'))
+    GUI = Window()
+    sys.exit(app.exec_())
+
+if __name__ == "__main__":
+    main()
