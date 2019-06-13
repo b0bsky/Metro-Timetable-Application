1065497e3c2ab9d77a01e032333cae7e9b130577
bobbutnotbob <37612781+bobbutnotbob@users.noreply.github.com>
2019-06-13 22:16:25 +1200

    added directions API
    Modified an example from the Google Maps API website to test drawing routes between bus stops

--- map-program.py
+++ map-program.py
@@ -5,12 +5,14 @@ from PyQt5.QtWebEngineWidgets import QWebEngineView
 sys.argv.append("--disable-web-security")
 app = QApplication(sys.argv)
 
+transport = "TRANSIT"
 raw_html = '''
+<!DOCTYPE html>
 <html>
   <head>
     <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
     <meta charset="utf-8">
-    <title>Simple Markers</title>
+    <title>Travel Modes in Directions</title>
     <style>
       /* Always set the map height explicitly to define the size of the div
        * element that contains the map. */
@@ -23,26 +25,56 @@ raw_html = '''
         margin: 0;
         padding: 0;
       }
+      #floating-panel {
+        position: absolute;
+        top: 10px;
+        left: 25%;
+        z-index: 5;
+        background-color: #fff;
+        padding: 5px;
+        border: 1px solid #999;
+        text-align: center;
+        font-family: 'Roboto','sans-serif';
+        line-height: 30px;
+        padding-left: 10px;
+      }
     </style>
   </head>
   <body>
     <div id="map"></div>
     <script>
       function initMap() {
-        var myLatLng = {lat: -25.363, lng: 131.044};
+        var directionsDisplay = new google.maps.DirectionsRenderer;
+        var directionsService = new google.maps.DirectionsService;
         var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 4,
-          center: myLatLng
+          zoom: 14,
+          center: {lat: -43.53425805, lng: 172.6370746}
         });
-        var marker = new google.maps.Marker({
-          position: myLatLng,
-          map: map,
-          title: 'Hello World!'
+        directionsDisplay.setMap(map);
+		
+        calculateAndDisplayRoute(directionsService, directionsDisplay);
+    
+      }
+
+      function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+        directionsService.route({
+          origin: {lat: -43.64270682, lng: 172.4676911},  // Lincoln University.
+          destination: {lat: -43.53425805, lng: 172.6370746},  // Bus Interchange.
+          // Note that Javascript allows us to access the constant
+          // using square brackets and a string value as its
+          // "property."
+          travelMode: google.maps.TravelMode['TRANSIT']
+        }, function(response, status) {
+          if (status == 'OK') {
+            directionsDisplay.setDirections(response);
+          } else {
+            window.alert('Directions request failed due to ' + status);
+          }
         });
       }
     </script>
     <script async defer
-    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBHB2ql6zLgZJXjIclYKHc_MSDB_cpUIag&callback=initMap">
+    src="https://maps.googleapis.com/maps/api/js?key=REDACTED">
     </script>
   </body>
 </html>
